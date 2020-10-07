d559ae635a78159325296c8bb8ddaf77ec2eac4a
sangjin1993 <hardoner@naver.com>
2020-10-07 19:11:57 +0900

    TheJavaTest project first commit

---
+++ src/main/java/me/whiteship/infearnthejavatest/InfearnTheJavaTestApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.infearnthejavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfearnTheJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfearnTheJavaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/whiteship/infearnthejavatest/InfearnTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.infearnthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfearnTheJavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
